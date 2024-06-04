169bce1c2985fc9a6d203748a4a2c52d1c855c5b
Archanabr2120 <79737184+Archanabr2120@users.noreply.github.com>
2024-06-04 09:31:13 +0530

    3.a)Command line tool to find  area of circle

--- main.c
+++ main.c
@@ -1,55 +1,35 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-int main() {
-    char word[] = "table";
-    char input[6];
-    int attempt;
+int main(int argc, char *argv[]) {
+    int opt;
+    float r, d, c, area;
 
-    for (attempt = 1; attempt <= 5; attempt++) {
-        printf("Enter your guess (attempt %d/5): ", attempt);
-        scanf("%5s", input);
 
-        if (strlen(input) != 5) {
-            printf("Invalid! \n");
-            attempt--;
-            continue;
-        }
 
-        if (strcmp(word, input) == 0) {
-            printf("'%s' correctly!\n", word);
-            break;
-        } else {
-            for(int i=0;i<5;i++)
-            {
-                for(int j=i;j<5;j++)
-                {
-                    if(word[i]==input[j])
-                       {
-                          if(i==j)
-                          {
-                            printf("%c is in correct position\n",input[j]);
-                            break;
-                          }
-                          else
-                          {
-                              printf("%c is in word but not in this position\n",input[j]);
-                              break;
-                          }
-
-                       }
-                       else
-                       {
-                           printf("%c is not in word\n",input[j]);
-                           break;
-                       }
-                }
-            }
+    while ((opt = getopt(argc, argv, "r:d:c:")) != -1) {
+        switch (opt) {
+            case 'r':
+                r = atof(optarg);
+                area = 3.14 * (r * r);
+                break;
+            case 'd':
+                d = atof(optarg);
+                area = 3.14 * ((d / 2) * (d / 2));
+                break;
+            case 'c':
+                c = atof(optarg);
+                area = (c * c) / (4 * 3.14);
+                break;
         }
     }
 
-    if (attempt > 5) {
-        printf("out of attempts! The correct word was: %s\n", word);
+    if (area > 0.0) {
+        printf("Area of the circle is %f\n", area);
+    } else {
+        printf("No valid option provided");
+        return 1;
     }
 
     return 0;
