d496e62f9c48f24a10da8ca122397dd74ddf58fd
Archanabr2120 <79737184+Archanabr2120@users.noreply.github.com>
2024-06-04 09:29:13 +0530

    1. Wordle game c mini project

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char word[] = "table";
+    char input[6];
+    int attempt;
+
+    for (attempt = 1; attempt <= 5; attempt++) {
+        printf("Enter your guess (attempt %d/5): ", attempt);
+        scanf("%5s", input);
+
+        if (strlen(input) != 5) {
+            printf("Invalid! \n");
+            attempt--;
+            continue;
+        }
+
+        if (strcmp(word, input) == 0) {
+            printf("'%s' correctly!\n", word);
+            break;
+        } else {
+            for(int i=0;i<5;i++)
+            {
+                for(int j=i;j<5;j++)
+                {
+                    if(word[i]==input[j])
+                       {
+                          if(i==j)
+                          {
+                            printf("%c is in correct position\n",input[j]);
+                            break;
+                          }
+                          else
+                          {
+                              printf("%c is in word but not in this position\n",input[j]);
+                              break;
+                          }
+
+                       }
+                       else
+                       {
+                           printf("%c is not in word\n",input[j]);
+                           break;
+                       }
+                }
+            }
+        }
+    }
+
+    if (attempt > 5) {
+        printf("out of attempts! The correct word was: %s\n", word);
+    }
+
+    return 0;
+}
